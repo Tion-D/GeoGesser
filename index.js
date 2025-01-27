de90a794461308c9e133a184de6630c65d27dac5
Tion_D <84892657+Tion-D@users.noreply.github.com>
2025-01-26 20:27:26 -0800

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const distanceRow = document.querySelector("#distanceRow");
 const replayButton = document.querySelector('#replay');
 const scoreTDs = ScoreRow.querySelectorAll("td");
 const distanceTDs = distanceRow.querySelectorAll("td");
+const endGame1 = document.querySelector("#endGame1");
+
+let timer;
 let overAllScore = 0;
 let round = 1;
 let overAllDistance = 0;
@@ -36,6 +39,10 @@ map.setView([0, 0], 3);
 let map2 = new L.Map('map2', {
 });
 playButton.addEventListener("click", function(){
+    timer = setTimeout(function(){
+        console.log("began");
+        submitGuess(null);
+    }, 120000);
     playButton.classList.add("hide");
     gameSection.classList.remove("hide");
 });
@@ -56,13 +63,13 @@ map3.setView([0, 0], 2.5);
 var {Viewer} = mapillary;
 
 let locations = [
-    {lat: 36.8214812, lng: -120.542477},
-    {lat: 39.734612815000446, lng: -95.024128265461},
-    {lat: 44.356075933746524, lng: 22.056555378580015},
-    {lat: 48.83112678675434, lng: 2.4012757852218556},
-    {lat: 53.1517718834, lng: 22.400322247339},
+    {lat: 10.038531829941618, lng: -68.71121452265015, imgID: 1352123905253316},
+    {lat: 17.394059371484104, lng: -57.08270248986122, imgID: 1350414852285153},
+    {lat: -1.6949823958166945, lng: -4.328393901845743, imgID: 327084939716423},
+    {lat: -0.5096138053929167, lng: 122.61402619859211, imgID: 339795405423817},
+    {lat: 32.87923517014107, lng: 124.14070375249912, imgID: 1187850948553139}
 ];
-let currentLocation = locations[Math.floor(Math.random() * locations.length)];
+let currentLocation = locations.splice(Math.floor(Math.random()*locations.length), 1)[0];
 actualCord.push(currentLocation);
 map2.setView([currentLocation.lat, currentLocation.lng], 6);
 
@@ -71,17 +78,24 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map2);
 
-let markerMap2 = L.marker([currentLocation.lat, currentLocation.lng]).addTo(map2);
+let markerMap2 = L.marker([currentLocation.lat, currentLocation.lng], {
+    icon: L.divIcon({ 
+        className: 'custom-icon', 
+        html: `<div style="background-color: ${colors[0]}; width: 25px; height: 25px; border-radius: 0%;"></div>`
+    })
+}).addTo(map2);
 
 var viewer = new Viewer({
     accessToken: 'MLY|8902114309901053|f68dc1e372116b667d9531c9e883c58c',
     container: 'mly',
-    imageId: '1686345301575128',
+    imageId: currentLocation.imgID,
 });
-
-function startGame(){
-    viewer.showNode(currentLocation.lat, currentLocation.lng);
-}
+let guessMarkerMap2 = L.marker([currentLocation.lat, currentLocation.lng], {
+    icon: L.divIcon({ 
+        className: 'custom-icon', 
+        html: `<div style="background-color: ${colors[0]}; width: 25px; height: 25px; border-radius: 50%;"></div>`
+    })
+}).addTo(map2);
 
 function calculateDistance(guessLat, guessLon, targetLat, targetLon){
     let radius = 6371;
@@ -123,42 +137,55 @@ function submitGuess(playerGuess){
         overAllScore += currentScore;
         overAllDistance += distance;
         guess.classList.remove("hide");
+        noGuess.classList.add("hide");
         scoreSpan.innerText = currentScore;
         distanceSpan.innerText = distance;
-        let guessMarkerMap2 = L.marker([playerGuess.lat, playerGuess.lng], {
-            icon: L.divIcon({ 
-                className: 'custom-icon', 
-                html: '<div style="background-color: red; width: 25px; height: 25px; border-radius: 50%;"></div>'
-            })
-        }).addTo(map2);
+        guessMarkerMap2.setLatLng([playerGuess.lat, playerGuess.lng]);
+        guessMarkerMap2._icon.innerHTML = `<div style="background-color: ${colors[round - 2]}; width: 25px; height: 25px; border-radius: 50%;"></div>`;
     }
     else{
+        guess.classList.add("hide");
         noGuess.classList.remove("hide");
         scoreSpan.innerText = 0;
         scoreValue.push(0);
         distanceValue.push("N/A");
+        guessMarkerMap2.setLatLng([currentLocation.lat, currentLocation.lng]);
+        guessMarkerMap2._icon.innerHTML = `<div style="background-color: ${colors[round - 2]}; width: 25px; height: 25px; border-radius: 50%;"></div>`;
     }
 }
 
 function changeLocation() {
     let newLocation;
     do {
-        newLocation = locations[Math.floor(Math.random() * locations.length)];
+        newLocation = locations.splice(Math.floor(Math.random()*locations.length), 1)[0];
     } while (newLocation.lat === currentLocation.lat && newLocation.lng === currentLocation.lng);
     actualCord.push(newLocation);
     currentLocation = newLocation;
     markerMap2.setLatLng([currentLocation.lat, currentLocation.lng]);
 
     map2.setView([currentLocation.lat, currentLocation.lng], 6);
-
+    if (viewer){
+        viewer.remove();
+        console.log("change locltion");
+    }
+    var viewer = new Viewer({
+        accessToken: 'TOKEN',
+        container: 'mly',
+        imageId: currentLocation.imgID,
+    });
+    markerMap2._icon.innerHTML = `<div style="background-color: ${colors[round - 1]}; width: 25px; height: 25px; border-radius: 0%;"></div>`;
 }
 
 function resetRound(){
+    timer = setTimeout(function(){
+        console.log("resetted");
+        submitGuess(null);
+    }, 120000);
     resultSection.classList.add("hide");
     gameSection.classList.remove("hide");
     if(round >= 3){
-        NextRoundButton.classList.add("#hide");
-        endGameButton.classList.remove("#hide");
+        NextRoundButton.classList.add("hide");
+        endGameButton.classList.remove("hide");
     }
     roundNumberDiv.innerText = round;
 }
@@ -203,34 +230,22 @@ function endGame(){
         return div;
     }
     legend.addTo(map3);
-    
+    endGame1.classList.remove("hide");
+    resultSection.classList.add("hide");
 }
+
+
 NextRoundButton.addEventListener("click", resetRound);
 endGameButton.addEventListener("click", endGame);
 replayButton.addEventListener("click", function(event){
     window.location.reload();
 });
 
-
-//startGame();
-
-/*
-overAllScore = 35;
-
-scoreValue = [25, 0, 10];
-overAllDistance = 520;
-
-distanceValue = [500, "N/A", 20];
-
-actualCord = [
-    {lat: 36.8214812, lng: -120.542477},
-    {lat: 39.734612815000446, lng: -95.024128265461},
-    {lat: 44.356075933746524, lng: 22.056555378580015}];
-
-playerGuessCord = [
-    {lat: 12.214812, lng: -51.42477},
-    {lat: 13.34612815000446, lng: -13.24128265461},
-    {lat: 11.56075933746524, lng: 51.56555378580015}];
-
-endGame();
-*/
+map.on('click', function(event){
+    clearTimeout(timer);
+    const playerGuessLocation = {
+        "lat": event.latlng.lat,
+        "lng": event.latlng.lng
+    };
+    submitGuess(playerGuessLocation);
+});
